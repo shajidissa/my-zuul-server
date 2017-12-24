000a89b2a8a3084ef80a8c2fe53a684a26649fac
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2017-12-24 01:49:12 +0000

    adding auth server

---
+++ src/main/java/uk/co/h2ss/myzuulserver/CorsConfig.java
@@ -0,0 +1,43 @@
+package uk.co.h2ss.myzuulserver;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+//private String allowOrigin= "http://localhost:8085";
+	
+// https://github.com/spring-projects/spring-security-oauth/issues/938
+
+@Bean
+public FilterRegistrationBean corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    
+    CorsConfiguration configAutenticacao = new CorsConfiguration();
+    configAutenticacao.setAllowCredentials(true);
+    //configAutenticacao.addAllowedOrigin(allowOrigin);
+    configAutenticacao.addAllowedOrigin("*");  
+    configAutenticacao.addAllowedHeader("Authorization");
+    configAutenticacao.addAllowedHeader("Content-Type");
+    configAutenticacao.addAllowedHeader("Accept");
+    configAutenticacao.addAllowedMethod("POST");
+    configAutenticacao.addAllowedMethod("GET");
+    configAutenticacao.addAllowedMethod("DELETE");
+    configAutenticacao.addAllowedMethod("PUT");
+    configAutenticacao.addAllowedMethod("OPTIONS");
+    configAutenticacao.setMaxAge(3600L);
+    source.registerCorsConfiguration("/oauth/token", configAutenticacao);
+    // source.registerCorsConfiguration("/**", configAutenticacao); // Global for all paths
+    
+    FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    return bean;
+}
+}
