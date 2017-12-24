0ed3141da58480ef1a6531495a741104301747c6
Shajid Issa <shajidissa@Shajids-MacBook-Pro.local>
2017-12-24 05:06:23 +0000

    adding auth server

--- src/main/java/uk/co/h2ss/myzuulserver/CorsConfig.java
+++ src/main/java/uk/co/h2ss/myzuulserver/CorsConfig.java
@@ -27,7 +27,6 @@ public FilterRegistrationBean corsFilter() {
     configAutenticacao.addAllowedHeader("Authorization");
     configAutenticacao.addAllowedHeader("Content-Type");
     configAutenticacao.addAllowedHeader("Accept");
-    //configAutenticacao.setAllowCredentials(true); 
     //configAutenticacao.addAllowedHeader("*");
     configAutenticacao.addAllowedMethod("POST");
     configAutenticacao.addAllowedMethod("GET");
@@ -35,8 +34,8 @@ public FilterRegistrationBean corsFilter() {
     configAutenticacao.addAllowedMethod("PUT");
     configAutenticacao.addAllowedMethod("OPTIONS");
     configAutenticacao.setMaxAge(3600L);
-    // source.registerCorsConfiguration("/oauth/token", configAutenticacao);
-    source.registerCorsConfiguration("/**", configAutenticacao); // Global for all paths
+    source.registerCorsConfiguration("/oauth/token", configAutenticacao);
+    // source.registerCorsConfiguration("/**", configAutenticacao); // Global for all paths
     
     FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
     bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
